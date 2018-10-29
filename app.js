4a414fc05d2243201f82637dc401dee9df9545e0
sec2018 <sec2018ilearn@163.com>
2018-10-29 16:48:24 +0800

    add  承运员

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ App({
     encryptedData: null,
     receiveAddress: null, //下单时，保存地址时调用
     address:null, //修改地址时调用
-    // apiRoot: 'http://wzjshuye.cn:8080'
-    apiRoot: 'http://192.168.100.107:8080'
-    //apiRoot: 'http://10.84.8.247:8080'
+    //apiRoot: 'http://wzjshuye.cn:8080'
+    //apiRoot: 'http://192.168.100.107:8080'
+    apiRoot: 'http://10.84.8.106:8080'
   }
 })  
