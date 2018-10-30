30cf7572f5d09bcf947e85522ba213ef80945ef4
sec2018 <sec2018ilearn@163.com>
2018-10-30 23:24:55 +0800

    qqqq

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ App({
     receiveAddress: null, //下单时，保存地址时调用
     address:null, //修改地址时调用
     //apiRoot: 'https://wzjshuye.cn:8882'
-    //apiRoot: 'http://192.168.100.107:8882'
-    apiRoot: 'http://10.84.8.212:8882'
+    apiRoot: 'http://192.168.100.107:8882'
+    //apiRoot: 'http://10.84.8.212:8882'
   }
 })  
